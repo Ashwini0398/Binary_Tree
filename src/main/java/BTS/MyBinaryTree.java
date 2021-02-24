ee5014a8ba7408ef0de147cbd2f220f5a667f2dc
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-02-24 18:51:32 +0530

    ADDED: Ability to search 63 in binary tree

--- src/main/java/BTS/MyBinaryTree.java
+++ src/main/java/BTS/MyBinaryTree.java
@@ -2,13 +2,14 @@ package BTS;
 
 public class MyBinaryTree<K extends Comparable<K>> {
     private MyBinaryNode<K> root;
+    private boolean resultToBeReturned = false;
+
 
     public void add(K key) {
         this.root = this.addRecursively(root, key);
     }
 
     private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
-
         if (current == null) {
             return new MyBinaryNode<>(key);
         }
@@ -29,6 +30,31 @@ public class MyBinaryTree<K extends Comparable<K>> {
 
     private int getSizeRecursive(MyBinaryNode<K> current) {
         return current == null ? 0 : 1 + this.getSizeRecursive(current.left) + this.getSizeRecursive(current.right);
+    }
+    public boolean search(K key) {
+        boolean result = this.searchRecursively(this.root, key);
+        if (resultToBeReturned == true) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean searchRecursively(MyBinaryNode<K> current, K key) {
+        if (current == null) {
+            return false;
+        }
+
+        int compareResult = key.compareTo(current.key);
+        if (compareResult == 0) {
+            this.resultToBeReturned = true;
+            return true;
+        }
+        if (compareResult < 0) {
+            searchRecursively(current.left, key);
+        } else {
+            searchRecursively(current.right, key);
+        }
+        return false;
     }
 
 }
